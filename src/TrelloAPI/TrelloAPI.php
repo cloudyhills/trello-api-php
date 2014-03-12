cf431aa7f8032591cecf65c8f4306b72694bf210
Ed Giese <edgiese@gmail.com>
2014-03-12 03:20:49 +0000

    fix Argument Order

--- src/TrelloAPI/TrelloAPI.php
+++ src/TrelloAPI/TrelloAPI.php
@@ -7,7 +7,22 @@
 namespace \TrelloAPI;
 
 class TrelloAPI {
-    static private $bPublicOnly = True;
+    protected $bPublicOnly = True;
+    protected $api_key;
+    protected $token;
 
-    function __construct(
+    function __construct(string $api_key, string $api_secret = null, string $token = null, string $token_secret = null) {
+        if (isset($api_key) && isset($api_secret) && isset($token_secret)) {
+            // oauth
+
+        } else if (isset($api_key)) {
+            // old-style non-OAuth
+        }
+
+        if (isset($token))
+            $this->bPublicOnly = False;
+
+        $this->api_key = $api_key;
+        $this->token = $token;
+    }
 }
